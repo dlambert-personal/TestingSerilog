42fe102c912d52c6213b094afac21df4278e3213
David Lambert <dlambert@appdev.info>
2022-11-16 18:27:22 -0500

    Fix file / folder names

--- DemoRunner/Worker.cs
+++ DemoRunner/Worker.cs
@@ -4,7 +4,6 @@ namespace FaimDemoRunner
 {
     public class Worker : BackgroundService
     {
-        //private readonly Serilog.ILogger<Worker> _logger;
         private readonly Serilog.ILogger _logger;
 
         public Worker(Serilog.ILogger logger)
@@ -18,13 +17,16 @@ namespace FaimDemoRunner
             { 
                 _logger.Information("Worker running at: {time}", DateTimeOffset.Now);
 
-                //var faim = new FAIMlib.FileReader(_logger);
-                // ? File.Exists(@"C:\Users\dlamb\source\repos\FAIMdemo\FAIM file.txt");
-                //faim.ParseRawRecs(@"FAIM file.txt");
-                //faim.ParseRawRecs(@".\..\..\..\..\..\FAIM file.txt");
 
                 await Task.Delay(1000, stoppingToken);
             }
+        }
+
+        public void TestMethod()
+        {
+            _logger.Information("Info message");
+            _logger.Warning("Warning message");
+            _logger.Error("Error message");
         }
     }
 }
